8d585786b94a59b29c91f0925e82c7c7e4c1d68c
Arm Bun <bellotaophyc@outlook.com>
2022-08-16 15:05:20 +0000

    migrate the single server I earlier created

--- stage/services/webserver-cluster/outputs.tf
+++ stage/services/webserver-cluster/outputs.tf
@@ -1,10 +1,4 @@
-
-    output "public_ip" {
-      value = aws_instance.warmup.public_ip
-      description = "The public IP address of the web server"
-    }
-
-    output "alb_dns_name" {
+   output "alb_dns_name" {
         value = aws_lb.warmup-asg.dns_name
         description = "The domain name of the load balancer"
-    }
+  }
